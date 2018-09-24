810a941335ad0c401527c2f3d3843ecf66ca0add
OrangeJewc <samnayerman@gmail.com>
2018-09-24 12:22:40 -0700

    Fix config

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
         display: 'minimal-ui',
         icon: 'src/images/logo.png', // This path is relative to the root of the site.
       },
-    },
-    'gatsby-plugin-offline',
+    }
+   //  'gatsby-plugin-offline',
   ],
 }
